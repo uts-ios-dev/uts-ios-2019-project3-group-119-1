524bfb30bac8ef8d4fccea8519fff893cf48915b
HochungWong <hochungwong@HochungWongs-MacBook-Pro.local>
2019-05-17 16:54:34 +1000

    fix storyboards

---
+++ password-manager/password-manager/View Controller/MainViewControlloer/MainTabBarController.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabBarController.swift
+//  password-manager
+//
+//  Created by HochungWong on 17/5/19.
+//  Copyright © 2019 HochungWong. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.navigationItem.hidesBackButton = true
+    }
+}
